8b1b83f2f55a512964d3f12fcf324cac75e22f35
David Choi <daaave@gmail.com>
2018-10-27 06:24:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,5 +87,40 @@ class TicTacToe
   end
   
   
+  def won?(board)
+    winning_positions = nil
   
+    WIN_COMBINATIONS.each do |combination|
+      c1 = combination[0]
+      c2 = combination[1]
+      c3 = combination[2]
+      
+      if(position_taken?(board, c1))
+        b1 = board[c1]
+        b2 = board[c2]
+        b3 = board[c3]
+      
+        row_symbol = b1
+        if row_symbol == b2 && row_symbol == b3
+          winning_positions = combination        
+        end
+      end
+    end
+    
+    return winning_positions
+  end
+  
+  def full?(board)
+    (0..8).all? do |index|
+      position_taken?(board, index)
+    end
+  end
+  
+  def draw?(board)
+    return full?(board) && !won?(board)
+  end
+  
+  def over?(board)
+    return draw?(board) || won?(board)
+  end
 end
